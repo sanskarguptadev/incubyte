aa753cbe19f6448b7533fbeac594b66927f9a60e
Sanskar Gupta <sanskar.gupta@getir.com>
2024-07-25 20:15:02 +0530

    code for all the cases

--- task.js
+++ task.js
@@ -3,9 +3,16 @@ class StringCalculator {
         if (numbers === "") {
             return 0;
         }
-        let delimiter = /,/;
+        let delimiter = /,|\n/;
+        if (numbers.startsWith("//")) {
+            const parts = numbers.split("\n", 2);
+            delimiter = new RegExp(parts[0].slice(2));
+            numbers = parts[1];
+        }
+
         const numberList = numbers.split(delimiter);
         let sumTotal = 0;
+        let negativeNumbers = [];
 
         for (let num of numberList) {
             if (num) {
@@ -15,6 +22,10 @@ class StringCalculator {
                 }
                 sumTotal += number;
             }
+        }
+
+        if (negativeNumbers.length > 0) {
+            throw new Error(`negative numbers not allowed: ${negativeNumbers.join(", ")}`);
         }
 
         return sumTotal;
@@ -27,6 +38,13 @@ const calculator = new StringCalculator();
 console.log(calculator.add("")); // 0
 console.log(calculator.add("1")); //1
 console.log(calculator.add("1,2,3")); //6
+console.log(calculator.add("//;\n1;2")); //3
+
+try {
+    console.log(calculator.add("1,-2,3")); // Error: negative numbers not allowed: -2
+} catch (e) {
+    console.error(e.message);
+}
 
 
 module.exports = StringCalculator;
